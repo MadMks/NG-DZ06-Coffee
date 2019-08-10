d0b7a66aa35e8ff61e613d0950d22cc4b7f7b43e
Max <mks59k@gmail.com>
2019-08-10 09:59:33 +0300

    Изменен подход к добавлению кофе на страницу (перед добавлением в корзину)

--- src/app/componentns/selected-coffee/selected-coffee.component.ts
+++ src/app/componentns/selected-coffee/selected-coffee.component.ts
@@ -12,24 +12,52 @@ export class SelectedCoffeeComponent implements OnInit, AfterContentChecked {
   id: string;
   currentCoffee = [];
   countCoffee = 1;
+  // countPortions = 1;
+  private lastCountCoffee = 1;
 
   constructor(private activeRoute: ActivatedRoute, private dataService: DataCoffeeService) {
     this.id = activeRoute.snapshot.params.id;
+
+    this.addCoffeeInOrder();
   }
 
   ngOnInit() {
 
   }
 
+  addCoffeeInOrder() {
+    const obj = this.dataService.getCoffeeObj(this.id);
+    obj['sugar'] = 0;
+    this.currentCoffee.push(obj);
+  }
   ngAfterContentChecked() {
-    this.currentCoffee = [];
-    for (let i = 0; i < this.countCoffee; i++) {
-      this.currentCoffee.push(this.dataService.getCoffeeObj(this.id));
+    // this.currentCoffee = [];
+    // for (let i = 0; i < this.countCoffee; i++) {
+    //   this.currentCoffee.push(this.dataService.getCoffeeObj(this.id));
+    // }
+  }
+
+  countChange() {
+    const objCoffee = this.dataService.getCoffeeObj(this.id);
+    console.log(1);
+    if ( this.lastCountCoffee > this.countCoffee) {
+      this.currentCoffee.splice(this.countCoffee);
+      console.log(2);
+    } else if (this.lastCountCoffee < this.countCoffee) {
+      // this.currentCoffee.push(objCoffee);
+      console.log(3);
+      this.addCoffeeInOrder();
     }
+    console.log(4);
+    this.lastCountCoffee = this.countCoffee;
   }
 
   addCard() {
     this.dataService.addOrderCoffee(this.currentCoffee);
     // this.router.navigate(['./']);
+  }
+
+  addSugar(count: number, index: number) {
+
   }
 }
